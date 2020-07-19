1983bf60daf9fe1eed69991f23a91dc332486ede
prashantsingh003 <shivamsinghss003@gmail.com>
2020-07-19 08:46:54 +0530

    updated shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('inputName') ingredientName;
+  @ViewChild('inputAmount') ingredientAmount;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addIngredient(){
+    const ingredientAdd = new Ingredient(this.ingredientName.nativeElement.value,this.ingredientAmount.nativeElement.value);
+    this.ingredientAdded.emit(ingredientAdd);
+  }
 }
